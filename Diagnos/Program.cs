dd5b035f1859d3dd5d7a6bcbed13bd7382a4df1b
teo erfass josefsson <teo.erfassjosefsson@elev.ga.ntig.se>
2024-10-22 13:02:41 +0200

    sdfynryu

---
+++ Diagnos/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("vällkomen!");
+
+Console.WriteLine("Hur många chanser vill du ha att gissa ordet [1-5]?");
+
+allt();
+
+
+
+
+
+
+static void allt(){
+string antgiss = Console.ReadLine();
+
+int resultat;
+int.TryParse(antgiss, out resultat);
+if (resultat >= 1 && resultat <= 5)
+{
+    for (; resultat > 0; resultat--)
+    {
+        Console.WriteLine("skriv din gissning:");
+        string gissning = Console.ReadLine();
+        if (gissning.ToLower() != "hund")
+        {
+            Console.WriteLine("Nej, det är fel!");
+        }
+        else if (gissning.ToLower() == "hund"){
+            Console.WriteLine("bra gissat, det var rätt Hund var det rätta ordet");
+        }
+        else if (resultat <= 0){
+            Console.WriteLine("tyvärr, du förlorade!");
+        }
+    }
+Console.WriteLine("Du förlorade tryck på ENTER för att avsluta!");
+Console.ReadLine();
+}
+}
